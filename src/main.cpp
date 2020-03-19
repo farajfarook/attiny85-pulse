1f280009ce5fe477ed12b96c498ef7f97f4b0092
Faraj Farook <farajfarook@gmail.com>
2020-03-19 18:02:13 +1100

    Introduct analog speed controller

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@
 #define CLOCK_MAX 10000
 #define CLOCK_MIN 50
 
-#define PIN_INCR PB3 // Interrupt pin of choice: Pin 3
-#define PIN_DECR PB4 // Interrupt pin of choice: Pin 3
-#define PIN_MODE PB2 // Pin2
-#define PIN_LED PB1  // Pin 1
-#define PIN_SIG PB0  // Pin 0
+#define PIN_SPEED PB3  // Pin 3
+#define PIN_MANUAL PB4 // Pin 4
+#define PIN_MODE PB2   // Pin 2
+#define PIN_LED PB1    // Pin 1
+#define PIN_SIG PB0    // Pin 0
 
 typedef bool ExecMode;
 #define MODE_AUTO 1
@@ -26,8 +26,8 @@ unsigned int clk_manual_exec = 0;
 int tick_auto = LOW;
 int tick_manual = LOW;
 
-int time_mode = 0, time_incr = 0, time_decr = 0;
-int data_mode, data_incr, data_decr;
+int time_mode = 0, time_manual = 0;
+int data_mode, data_manual, data_speed;
 
 void setup()
 {
@@ -35,8 +35,8 @@ void setup()
     pinMode(PIN_SIG, OUTPUT);
 
     pinMode(PIN_MODE, INPUT_PULLUP);
-    pinMode(PIN_INCR, INPUT_PULLUP);
-    pinMode(PIN_DECR, INPUT_PULLUP);
+    pinMode(PIN_MANUAL, INPUT_PULLUP);
+    pinMode(PIN_SPEED, INPUT);
 }
 
 ExecMode fetch_mode(int time)
@@ -51,31 +51,25 @@ ExecMode fetch_mode(int time)
     return data_mode == LOW ? MODE_MANUAL : MODE_AUTO;
 }
 
-bool is_incr_clicked(int time)
+void fetch_clk_manual(int time)
 {
-    int data_exec = digitalRead(PIN_INCR);
-    int time_diff = time - time_incr;
-    if (data_exec != data_incr && time_diff > DEBOUNCE_TIME)
+    int data_exec = digitalRead(PIN_MANUAL);
+    int time_diff = time - time_manual;
+    if (data_exec != data_manual && time_diff > DEBOUNCE_TIME)
     {
-        data_incr = data_exec;
-        time_incr = time;
+        data_manual = data_exec;
+        time_manual = time;
+        clk_manual_exec = 0;
     }
-    return data_incr == LOW;
 }
 
-bool is_decr_clicked(int time)
+void fetch_clk_auto(int time)
 {
-    int data_exec = digitalRead(PIN_DECR);
-    int time_diff = time - time_decr;
-    if (data_exec != data_decr && time_diff > DEBOUNCE_TIME)
-    {
-        data_decr = data_exec;
-        time_decr = time;
-    }
-    return data_decr == LOW;
+    int value = analogRead(PIN_SPEED);
+    clk_auto = value * 5;
 }
 
-void manual_clk_exec()
+void run_clk_manual()
 {
     if (clk_manual_exec >= clk_manual)
     {
@@ -88,15 +82,7 @@ void manual_clk_exec()
     }
 }
 
-void manual_clk_evt(bool incr, bool decr)
-{
-    if (decr || incr)
-    {
-        clk_manual_exec = 0;
-    }
-}
-
-void auto_clk_exec()
+void run_clk_auto()
 {
     if (clk_auto_exec >= clk_auto)
     {
@@ -109,44 +95,19 @@ void auto_clk_exec()
     }
 }
 
-void auto_clk_evt(bool incr, bool decr)
-{
-    if (incr)
-    {
-        clk_auto_exec = 0;
-        clk_auto += clk_auto / 4;
-        if (clk_auto > CLOCK_MAX)
-        {
-            clk_auto = CLOCK_MAX;
-        }
-    }
-    if (decr)
-    {
-        clk_auto_exec = 0;
-        clk_auto -= clk_auto / 4;
-        if (clk_auto < CLOCK_MIN)
-        {
-            clk_auto = CLOCK_MIN;
-        }
-    }
-}
-
 void loop()
 {
     int time = millis();
+
+    fetch_clk_auto(time);
+    fetch_clk_manual(time);
+
+    run_clk_auto();
+    run_clk_manual();
+
     ExecMode mode = fetch_mode(time);
-    bool decr = is_decr_clicked(time);
-    bool incr = is_incr_clicked(time);
-
-    if (mode == MODE_AUTO)
-        auto_clk_evt(incr, decr);
-    else
-        manual_clk_evt(incr, decr);
-
-    auto_clk_exec();
-    manual_clk_exec();
-
     digitalWrite(PIN_SIG, mode == MODE_AUTO ? tick_auto : tick_manual);
     digitalWrite(PIN_LED, tick_auto);
+
     delay(1);
 }
